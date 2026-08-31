a2598a5828e2f3551a0344f97fe25af7c8306c3e
vaibhav <vaibhav.parab@softlabsgroup.com>
2026-08-31 15:37:36 +0530

    changes

--- Frontend/src/API/ServiceURl.js
+++ Frontend/src/API/ServiceURl.js
@@ -1,4 +1,4 @@
 export const ServiceURL = {
    // API_URL: "http://localhost:8000", 
-   API_URL: "https://new-portfolio-1-wg9i.onrender.com/"
+   API_URL: "https://new-portfolio-1-wg9i.onrender.com"
 };
